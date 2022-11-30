06f5deb6e7217f951c81bc9e966f01723409848c
Eunyoung Bang <e_yeong@naver.com>
2022-11-30 11:35:50 +0900

    Add visualization code

--- train_valid_maker.py
+++ train_valid_maker.py
@@ -3,30 +3,55 @@ import os
 import numpy as np
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
+
 # file path
-# 낙엽송
-larch = "../Dataset/Sample/larch/"
+# 낙엽송 
+larch = "./larch/"
 # 잣나무
-pine = "../Dataset/Sample/pine/"
+pine = "./pine/"
 
 # slow is 0, fast is 1
-def make_dataframe(path, train_valid, species):
-    fname_list = os.listdir(path)
-   
-    fname_list = [species+"/"+i+"\n" for i in fname_list]
-    return fname_list
+def make_dataframe(path, species):
+    
+    fname_list = [file for file in os.listdir(path) if file.endswith(".ply")]
+
+    X_train, X_val= train_test_split(fname_list, test_size=0.2, random_state=42)
+    X_val, X_test= train_test_split(X_val, test_size=0.5, random_state=42)
+
+    X_train = [species+"/"+i+"\n" for i in X_train]
+    X_val = [species+"/"+i+"\n" for i in X_val]
+    X_test = [species+"/"+i+"\n" for i in X_test]
+    return X_train, X_val, X_test
     
 
-larch_txt = make_dataframe(larch, "train", "larch")
-pine_txt = make_dataframe(pine, "train", "pine")
+larch_train, larch_val, larch_test = make_dataframe(larch, "larch")
+pine_train, pine_val, pine_test = make_dataframe(pine, "pine")
 
-print(larch_txt)
-
-f = open("../Dataset/Sample/trainval.txt", 'w')
-for i in larch_txt:
+# train file
+f = open("./Sample/train.txt", 'w')
+for i in larch_train:
     f.write(i)
 
-for i in pine_txt:
+for i in pine_train:
+    f.write(i)
+f.close()
+
+# valid file
+f = open("./Sample/valid.txt", 'w')
+for i in larch_val:
+    f.write(i)
+
+for i in pine_val:
+    f.write(i)
+f.close()
+
+# test file
+f = open("./Sample/test.txt", 'w')
+for i in larch_test:
+    f.write(i)
+
+for i in pine_test:
     f.write(i)
 f.close()
 
